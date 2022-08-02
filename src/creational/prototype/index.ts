06a1c26e8d5eb4529abc870b94517742a2c40840
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-08-02 12:54:04 -0300

    refactor: improve prototype pattern example

--- src/creational/prototype/index.ts
+++ src/creational/prototype/index.ts
@@ -1,15 +1,22 @@
+/* eslint-disable no-proto */
+import assert from 'assert'
 import { Developer } from './concrete/developer-prototype'
 
-(() => {
-  const developer = new Developer('Eduardo', '21', 'node')
-  const otherDeveloper = developer.clone()
+const developer = new Developer('Eduardo', '21', 'node')
+const otherDeveloper = developer.clone()
 
-  console.log('Original developer: ')
-  console.log(developer)
-  console.log('name: ', developer.name)
-  console.log('age: ', developer.age)
-  console.log('Other developer: ')
-  console.log(otherDeveloper)
-  console.log('name: ', otherDeveloper.name)
-  console.log('age: ', otherDeveloper.age)
-})()
+// Logs
+console.log('-'.repeat(100))
+console.log('Original developer: ')
+console.log(developer)
+console.log('name: ', developer.name)
+console.log('age: ', developer.age)
+console.log('Other developer: ')
+console.log(otherDeveloper)
+console.log('name: ', otherDeveloper.name)
+console.log('age: ', otherDeveloper.age)
+console.log('-'.repeat(100))
+
+// Tests
+// @ts-ignore
+assert.deepEqual(developer, otherDeveloper.__proto__)
