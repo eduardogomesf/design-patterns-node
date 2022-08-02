2a1833af0114f6eda2d0f077e0c31979f3361155
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-08-02 11:00:55 -0300

    refactor: improve factory method pattern example

--- src/creational/factory/index.ts
+++ src/creational/factory/index.ts
@@ -1,17 +1,28 @@
+import assert from 'assert'
 import { DeveloperFactory } from './concrete/developer-factory'
 import { TesterFactory } from './concrete/tester-factory'
 import { ArchitectFactory } from './concrete/architect-factory'
 
-(() => {
-  const architectFactory = new ArchitectFactory()
-  const developerFactory = new DeveloperFactory()
-  const testerFactory = new TesterFactory()
+const architectFactory = new ArchitectFactory()
+const developerFactory = new DeveloperFactory()
+const testerFactory = new TesterFactory()
 
-  const developer = developerFactory.create('Eduardo', '21', 3000)
-  const tester = testerFactory.create('Lucas', '25', 6000)
-  const architect = architectFactory.create('João', '33', 11500)
+const developer = developerFactory.create('Eduardo', '21', 3000)
+const tester = testerFactory.create('Lucas', '25', 6000)
+const architect = architectFactory.create('João', '33', 11500)
 
-  console.log(developer.getInfo())
-  console.log(tester.getInfo())
-  console.log(architect.getInfo())
-})()
+const developerInfo = developer.getInfo()
+const testerInfo = tester.getInfo()
+const architectInfo = architect.getInfo()
+
+// Logs
+console.log('-'.repeat(100))
+console.log(developerInfo)
+console.log(testerInfo)
+console.log(architectInfo)
+console.log('-'.repeat(100))
+
+// Tests
+assert.equal(developerInfo, 'My name is Eduardo, I\'m a 21-year-old Developer. My currently monthly wage is 3000')
+assert.equal(testerInfo, 'My name is Lucas, I\'m a 25-year-old Tester. My currently monthly wage is 6000')
+assert.equal(architectInfo, 'My name is João, I\'m a 33-year-old Architect. My currently monthly wage is 11500')
